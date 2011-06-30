ac140315ba75454208cc9cd325a522d5effe153d
Maik Woehl <maik.woehl@web.de>
2011-06-30 13:55:03 +0200

    some bugfixes and some new files

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     description = "tkhelp is a german help program. Is implemented a GUI with many Tk-Widgets and a Webinterface, that is easy to update.",
     long_description = "The GUI is a small collection of Tk-Widget descriptions. In the Webinterface you can see many examples and a great description, because the GUI is only updated in the stable releases(1.0,1.5,2.0,2.5,...).",
     data_files = ['test.gif', 'CHANGELOG', 'README', 'TODO'],
-    py_modules = ['tkhelp', 'tkh_splash3', 'tkhelp_lang_de', 'dt_tkobjects']
+    py_modules = ['tkhelp', 'tkh_splash3', 'tkhelp_lang_de', 'dt_tkobjects', 'config']
     
 )
